a6404e6150c4e37803754266ccb0e6cc94b53022
Rodion Yaryy <ryaryy@gmail.com>
2015-04-28 16:07:30 -0700

    minor changes

--- javascripts/main.js
+++ javascripts/main.js
@@ -10,3 +10,30 @@ $('#jQueryBtn').click(function() {
 
 var el = document.getElementById("EventListener");
 el.addEventListener("click", buttonClick, false);
+
+var successClass = '.alert.alert-dismissible.alert-success';
+var dangerClass = '.alert.alert-dismissible.alert-danger';
+
+$(successClass).hide();
+$(dangerClass).hide();
+
+$('#SuccessBtn').click(function(){
+    var input = $('#inputSmall').val()
+
+    if(input) {
+        $(dangerClass).hide();
+        $(successClass).show(200);
+        $('#display').text(input);
+    }
+    else {
+        $(successClass).hide();
+        $(dangerClass).show(200);
+    }
+});
+
+$('.close').click(function(){
+   $(this).parent().hide(200);
+});
+
+
+
